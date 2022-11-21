f9c01194ac98a38bf09e7186dd30db9b93fb7591
montassaralaya <115627113+montassaralaya@users.noreply.github.com>
2022-11-21 23:32:20 +0100

    Add files via upload

--- pointu.c
+++ pointu.c
@@ -5,70 +5,75 @@ int ajouter(Utilisateur u , char filename [])
     FILE * f=fopen(filename, "a");
     if(f!=NULL)
     {
-        fprintf(f,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
+        fprintf(f,"%d %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
         fclose(f);
         return 1;
     }
     else return 0;
 }
 
-int modifier(int loginu, Utilisateur nouv, char * filename)
+int modifier(int loginu, Utilisateur nouv, char *filename)
 {
 Utilisateur u;
+int tr=0;
     FILE * f=fopen(filename, "r");
     FILE * f2 =fopen("aux.txt", "w");
     if(f==NULL || f2==NULL)
-return 0;
-else
     {
-while(fscanf(f,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF);
+while(fscanf(f,"%d %s %s %s %s %s %d %d %d %d %d %s\n",&u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF)
 {
-if(u.loginu!=loginu)
-        fprintf(f2,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
+if(u.loginu!=loginu){
+        fprintf(f2,"%d %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
+tr=1;
+}
 else
 
-  fprintf(f2,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
+  fprintf(f2,"%d %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
 
+}
 }
         fclose(f);
         fclose(f2);
 remove(filename);
 rename("aux.txt", filename);
-        return 1;
-    }
-  
+return tr; 
 }
+ 
 
-int supprimer(int loginu, char * filename)
+int supprimer(int loginu, char *filename)
 {
 Utilisateur u;
+int tr=0;
     FILE * f=fopen(filename, "r");
     FILE * f2 =fopen("aux.txt", "w");
     if(f==NULL || f2==NULL)
-return 0;
-else
     {
-while(fscanf(f,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF);
+while(fscanf(f,"%d %s %s %s %s %s %d %d %d %d %d %s\n",&u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF)
 {
 if(u.loginu!=loginu)
-        fprintf(f2,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
+tr=1;
+else
+        fprintf(f2,"%d %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,u.DN.jours,u.DN.mois,u.DN.annee,u.numbv,u.voteu,u.roleu);
 
+}
 }
         fclose(f);
         fclose(f2);
 remove(filename);
 rename("aux.txt", filename);
-        return 1;
+        return tr;
     }
-}
 
-Utilisateur chercher(int loginu, char * filename)
+
+
+Utilisateur chercher(int loginu, char *filename)
 {
-Utilisateur u; int tr=0;
+Utilisateur u; 
+int tr;
     FILE * f=fopen(filename, "r");
  if(f!=NULL )
     {
-while(fscanf(f,"%s %s %s %s %s %s %d %d %d %d %d %s\n",u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF && tr==0)
+while(fscanf(f,"%d %s %s %s %s %s %d %d %d %d %d %s\n",&u.loginu,u.MPu,u.cinu,u.nomu,u.prenomu,u.genderu,&u.DN.jours,&u.DN.mois,&u.DN.annee,&u.numbv,&u.voteu,u.roleu)!=EOF && tr==0)
 {if(loginu==u.loginu)
 tr=1;
 }
